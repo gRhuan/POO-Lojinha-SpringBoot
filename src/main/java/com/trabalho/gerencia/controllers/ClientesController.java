5e793cd63b39502323518f8690cee728c4469e0e
Gladyson Rhuan <gladysonr04@gmail.com>
2024-11-07 21:40:36 -0300

    Dto

--- src/main/java/com/trabalho/gerencia/controllers/ClientesController.java
+++ src/main/java/com/trabalho/gerencia/controllers/ClientesController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.trabalho.gerencia.dto.RequisicaoNovoCliente;
 import com.trabalho.gerencia.models.Cliente;
 import com.trabalho.gerencia.repositories.ClientesRepository;
 
@@ -17,7 +19,7 @@ public class ClientesController {
     public List<Cliente> getAllClientes() {
         return clientesRepository.findAll();
     }
-    
+
     @GetMapping("/clientes")
     public ModelAndView index() {
         List<Cliente> clientes = this.getAllClientes();
@@ -27,10 +29,30 @@ public class ClientesController {
     }
 
     @GetMapping("/clientes/adicionar")
-    public ModelAndView novo() {
-        ModelAndView mv = new ModelAndView("clientes/adicionar");
-        return mv;
+    public String criar(RequisicaoNovoCliente requisicao) {
+        Cliente cliente = requisicao.toClientes();
+        this.clientesRepository.save(cliente);
+        return "redirect:/funcionarios";
     }
 
-   
+    @PostMapping("/clientes/atualizar/{id}")
+    public String updateCliente(@PathVariable Long id, @RequestBody RequisicaoNovoCliente requisicao) {
+        // Busca o cliente existente pelo ID
+        Cliente clienteExistente = clientesRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado: " + id));
+
+        // Atualiza os dados do cliente com as informações da requisição
+        clienteExistente.setNome(requisicao.getNome());
+        clienteExistente.setCpf(requisicao.getCpf());
+        clienteExistente.setEndereco(requisicao.getEndereco());
+        clienteExistente.setCidade(requisicao.getCidade());
+        clienteExistente.setBairro(requisicao.getBairro());
+        clienteExistente.setUf(requisicao.getUf());
+
+        // Salva as mudanças no banco de dados
+        clientesRepository.save(clienteExistente);
+
+        return "redirect:/clientes";
+    }
+
 }
